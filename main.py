4f4f21fb05d912d0388d2fad384b74fdb5914bd1
Dmitriy <115549265+M0R1S0N@users.noreply.github.com>
2023-09-14 17:53:43 +0000

    Added fields such as: 'Discount' and 'Full price'

--- main.py
+++ main.py
@@ -13,19 +13,17 @@ link = "https://book24.ru"+soup.find('div', class_='product-list__item').find('a
 name = soup.find('div', class_='product-list__item').find('a', class_='product-card__name').get('title')
 author = soup.find('div', class_='product-list__item').find('div', class_='product-card__authors-holder').text
 price = soup.find('div', class_='product-list__item').find('article').get('data-b24-price')
-#full_price = soup.find('div', class_='product-list__item').find('div', class_='product-card-price__old').find('span', class_='product-card-price__old-value').text
-#sale = soup.find('div', class_='product-list__item').find('div', class_='product-card-price__old').find('span', class_='product-card-price__discount').text
 
 
 data = []
 
 
-for p  in range(1, 7):
+for p in range(1, 7):
     print(p)
     
     url = f'https://book24.ru/knigi-bestsellery/page-{p}'
     r = requests.get(url)
-    sleep(3)
+    sleep(1)
     soup = BeautifulSoup(r.text, 'lxml')
 
 
@@ -37,17 +35,22 @@ for p  in range(1, 7):
         try:
             author = book.find('div', class_='product-card__authors-holder').text
             price = book.find('article').get('data-b24-price')
-            #full_price = book.find('div', class_='product-card-price__old').get('span')
-            #sale = book.find('div', class_='product-card-price__old').get('span')
         except AttributeError:
             author = ' - '
-            #full_price = ' - ' 
-            #sale = ' - '
-        data.append([link, name, author, price+' Руб'])
+        try:
+            full_price = price
+            sale = book.find('article').get('data-b24-discount')
+            if sale == '0':
+                sale = 0 
+                full_price = price
+            else:
+                full_price = int(sale) + int(price)
+        except:
+            sale = 0
+            full_price = price
+        data.append([link, name, author, int(price), int(sale), int(full_price)])
     
-header = ['Ссылка ', 'Название ', 'Автор ', 'Цена']
+header = ['Ссылка ', 'Название ', 'Автор ', 'Цена', 'Скидка','Полная стоимость']
 df = pd.DataFrame(data, columns=header)
 df.to_csv('C:/Users/ryazantsev/Desktop/data.csv', sep=';', encoding='utf-8')
-    
-            
     
